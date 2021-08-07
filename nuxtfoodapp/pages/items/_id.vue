d2ace2d444340c23534b3d1d489935727fac924f
ibrahim <dnzibrahim1@gmail.com>
2021-08-07 15:37:24 +0300

    Dynamic page solution

--- nuxtfoodapp/pages/items/_id.vue
+++ nuxtfoodapp/pages/items/_id.vue
@@ -1,7 +1,38 @@
 <template>
-  <div>
-    <pre>{{ currentItem }} </pre>
-  </div>
+  <main class="container">
+    <section
+      class="image"
+      :style="{
+        background: `rgba(0, 0, 0, 0) url(${itemImage}) no-repeat scroll center center;`
+      }"
+    ></section>
+    <section class="details">
+      <h2>{{ currentItem.item }}</h2>
+      <h3>Price: ${{ currentItem.price }}</h3>
+      <div class="quantity">
+        <input v-model="itemCount" type="number" min="1" />
+        <button class="primary">Add to Cart ${{ totalPrice }}</button>
+      </div>
+      <fieldset>
+        <legend><h3>Add Ons</h3></legend>
+        <div v-for="addOn in currentItem.addOns" :key="addOn">
+          <input
+            @change="e => (selectedAddOn = e.target.value)"
+            :id="addOn"
+            type="checkbox"
+            name="addon"
+            :value="addOn"
+            :checked="addOn === selectedAddOn ? 'true' : ''"
+          />
+          <label :for="addOn">{{ addOn }} </label>
+        </div>
+      </fieldset>
+    </section>
+    <section class="options">
+      <h3>Description</h3>
+      <p>{{ currentItem.description }}</p>
+    </section>
+  </main>
 </template>
 
 <script>
@@ -10,7 +41,9 @@ import { mapState } from "vuex";
 export default {
   data() {
     return {
-      id: this.$route.params.id
+      id: this.$route.params.id,
+      itemCount: 1,
+      selectedAddOn: ""
     };
   },
   computed: {
@@ -28,9 +61,45 @@ export default {
       }
 
       return result;
+    },
+    itemImage() {
+      return "/" + this.currentItem.img;
+    },
+    totalPrice() {
+      return Number(this.currentItem.price * this.itemCount).toFixed(2);
     }
   }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  width: 1000px;
+  margin: 100px auto;
+  display: grid;
+  grid-template-columns: 400px 1fr;
+  grid-template-rows: 400px 1fr;
+  grid-column-gap: 60px;
+  grid-row-gap: 60px;
+  line-height: 2;
+}
+
+.image {
+  grid-area: 1 / 1 / 2 / 2;
+}
+.details {
+  grid-area: 1 / 2 / 2 / 3;
+  position: relative;
+}
+.options {
+  grid-area: 2 / 1 / 3 / 2;
+}
+.quantity {
+  display: flex;
+  margin: 20px 0 40px;
+}
+fieldset {
+  margin: 20px 0;
+  border: 1px solid #ddd;
+}
+</style>
