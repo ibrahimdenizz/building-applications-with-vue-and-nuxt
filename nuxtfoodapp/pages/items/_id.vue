2e504a3562c7da8bfe400f4d9025e8b77470b287
ibrahim <dnzibrahim1@gmail.com>
2021-08-07 13:17:08 +0300

    Create a dynamic page

---
+++ nuxtfoodapp/pages/items/_id.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <pre>{{ currentItem }} </pre>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  data() {
+    return {
+      id: this.$route.params.id
+    };
+  },
+  computed: {
+    ...mapState(["foodData"]),
+    currentItem() {
+      let result;
+
+      for (let i = 0; i < this.foodData.length; i++) {
+        for (let j = 0; j < this.foodData[i].menu.length; j++) {
+          if (this.foodData[i].menu[j].id === this.id) {
+            result = this.foodData[i].menu[j];
+            break;
+          }
+        }
+      }
+
+      return result;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
