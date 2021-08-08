fde10b8b372a0d4a02e50ac1c1304eae6de2b550
ibrahim <dnzibrahim1@gmail.com>
2021-08-08 15:47:30 +0300

    composition API solution

---
+++ compapi-start/pages/tacofriends.vue
@@ -0,0 +1,53 @@
+<template>
+  <main>
+    <h1>Our Taco Friends:</h1>
+    <ul>
+      <li v-for="friend in friends" :key="friend" @click="clickFriend(friend)">
+        {{ friend }}
+      </li>
+    </ul>
+    <p><i>Click on a friend!</i></p>
+  </main>
+</template>
+
+<script>
+import { reactive, toRefs } from "@vue/composition-api";
+
+export default {
+  setup() {
+    const state = reactive({
+      friends: [
+        "Sarah likes all tacos",
+        "Brian Holt likes whiskey more than tacos",
+        "Shirley Wu likes tacos as much as Wonder Woman"
+      ]
+    });
+
+    function clickFriend(friend) {
+      alert(friend);
+    }
+
+    return {
+      ...toRefs(state),
+      clickFriend
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+ul {
+  width: 400px;
+}
+li {
+  text-align: left;
+  transition: all 0.4s ease;
+  cursor: pointer;
+}
+li:hover {
+  background-color: #eee;
+}
+i {
+  color: #555;
+}
+</style>
